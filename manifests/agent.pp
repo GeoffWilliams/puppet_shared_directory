3cf184bce823395ff9b8c3caaa54f1ae928994ee
admin <geoff.williams@puppetlabs.com>
2014-08-21 22:46:56 +0000

    wrong path

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -1,7 +1,7 @@
 # copy files from the puppet master to a directory on an agent
 class puppet_shared_directory::agent(
     $target_dir  = "/root/extra_files",
-    $mount_point = "shared"
+    $mount_point = "extra_files"
   ) {
 
   $pfs_uri = "puppet:///${mount_point}/"
